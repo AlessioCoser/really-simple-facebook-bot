e20d8339dfdafb6a6b5d71c54a8a96ad7b78b0e3
alessiocoser <alessio.coser@xpeppers.com>
2017-01-17 17:46:10 +0100

    limit to facebook platform

--- bot.js
+++ bot.js
@@ -5,4 +5,4 @@ module.exports = botBuilder(function (message) {
   return 'Thanks for sending ' + message.text +
     '.\nYour message is very important to us, but ' +
     excuse.get();
-});
+}, {platforms: ['facebook']});
