d535236d9e0c1aad37cd8d267ae619354deae0ba
alessiocoser <alessio.coser@xpeppers.com>
2017-01-17 17:27:08 +0100

    first commit for really simple bot

---
+++ bot.js
@@ -0,0 +1,8 @@
+const botBuilder = require('claudia-bot-builder');
+const excuse = require('huh');
+
+module.exports = botBuilder(function (message) {
+  return 'Thanks for sending ' + message.text +
+    '.\nYour message is very important to us, but ' +
+    excuse.get();
+});
