2aa80f1fcfdd83d9492a98d522bd3b4f4d3275e4
alessiocoser <alessio.coser@xpeppers.com>
2017-01-20 15:43:57 +0100

    remove unnecessary comma

--- bot.js
+++ bot.js
@@ -1,8 +1,8 @@
-const botBuilder = require('claudia-bot-builder');
-const excuse = require('huh');
+const botBuilder = require('claudia-bot-builder')
+const excuse = require('huh')
 
 module.exports = botBuilder(function (message) {
   return 'Thanks for sending ' + message.text +
     '.\nYour message is very important to us, but ' +
-    excuse.get();
-}, {platforms: ['facebook']});
+    excuse.get()
+}, {platforms: ['facebook']})
